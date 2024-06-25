eb3a85d86394e91cb06c6162a27b1876e80ea386
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-06-25 23:53:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version="0.1",
     packages=find_packages(),
     package_dir={"": "."},
+    include_package_data=True,
+    package_data={
+        'tts_asr_eval_suite.dnsmos': ['ckpt/DNSMOS/*.onnx', 'ckpt/pDNSMOS/*.onnx'],
+    },
     install_requires=[
         "torch",
         "fairseq",
