a1f61da5808b69b8a1628d99506fbd655db42107
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-06-26 13:21:25 +0900

    fix for incomplete model download errors using torch.hub

--- tts_asr_eval_suite/utmos/utmos.py
+++ tts_asr_eval_suite/utmos/utmos.py
@@ -1,4 +1,7 @@
+import os
+import requests
 import discrete_speech_metrics as dsm
+import torch
 import torchaudio
 
 
@@ -6,6 +9,14 @@ class UTMOS:
     def __init__(self, device) -> None:
         self.device = device
         self.sr = 16000
+
+        # This is fix because the hub.load throws error and downloads incomplete file
+        url = "https://github.com/tarepan/SpeechMOS/releases/download/v1.0.0/utmos22_strong_step7459_v1.pt"
+        local_path = os.path.join(torch.hub.get_dir(), 'checkpoints', 'utmos22_strong_step7459_v1.pt')
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+        with open(local_path, 'wb') as f:
+            f.write(requests.get(url).content)
+
         self.metric = dsm.UTMOS(sr=16000)
 
     def __call__(self, gt_audio_path, gen_audio_path):
