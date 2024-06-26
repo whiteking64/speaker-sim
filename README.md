845a7e2f6d96f6d6bbcabc95debb66646846fd27
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-06-26 13:02:47 +0900

    fix speechBLEU

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A suite of various automatic evaluation metrics for ASR, TTS and VC
 # Please first install numpy, pypesq and Cython before running setup.py
 
 ```shell
-pip install numpy
+pip install 'numpy<2'
 pip install pypesq@https://github.com/vBaiCai/python-pesq/archive/master.zip
 pip install Cython
 ```
--- tts_asr_eval_suite/SpeechBLEU/speech_bleu.py
+++ tts_asr_eval_suite/SpeechBLEU/speech_bleu.py
@@ -6,10 +6,17 @@ class SpeechBLEU:
     def __init__(self, device) -> None:
         self.device = device
         self.sr = 16000
-        self.metric = dsm.UTMOS(sr=16000)
+        self.metric = dsm.SpeechBLEU(
+            sr=16000,
+            model_type="hubert-base",
+            vocab=200,
+            layer=11,
+            n_ngram=2,
+            remove_repetition=True,
+            use_gpu=True
+        )
 
     def __call__(self, gen_audio_path):
-
         gen_audio, gen_sr = torchaudio.load(gen_audio_path)
 
         if gen_sr != self.sr:
