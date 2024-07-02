0082674426a818a4a4872e9fcf4e5f395213a474
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-07-02 17:06:34 +0900

    Update utmos.py

--- tts_asr_eval_suite/utmos/utmos.py
+++ tts_asr_eval_suite/utmos/utmos.py
@@ -19,22 +19,14 @@ class UTMOS:
 
         self.metric = dsm.UTMOS(sr=16000)
 
-    def __call__(self, gt_audio_path, gen_audio_path):
+    def __call__(self, gen_audio_path):
 
-        gt_audio, gt_sr = torchaudio.load(gt_audio_path)
         gen_audio, gen_sr = torchaudio.load(gen_audio_path)
 
-        if gt_sr != self.sr:
-            gt_audio = torchaudio.functional.resample(gt_audio, gt_sr, self.sr)
         if gen_sr != self.sr:
             gen_audio = torchaudio.functional.resample(gen_audio, gen_sr, self.sr)
 
-        gt_audio = gt_audio[0].numpy()
         gen_audio = gen_audio[0].numpy()
-
-        assert gt_audio.shape[0] == gen_audio.shape[0], \
-            (f"Audio files have different lengths: {gt_audio.shape[0]} != {gen_audio.shape[0]}, "
-             f"PESQ requires audio files to have the same length.")
 
         score = self.metric.score(gen_audio)
 
