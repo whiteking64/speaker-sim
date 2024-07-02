04c90c780348a2e836dd2c942d69f3e0b030077d
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-07-02 15:15:21 +0900

    Update __init__.py

--- tts_asr_eval_suite/__init__.py
+++ tts_asr_eval_suite/__init__.py
@@ -1,4 +1,4 @@
-from .asr_intelligibility.asr_intelligbility import ASRIntelligibility
+# from .asr_intelligibility.asr_intelligbility import ASRIntelligibility
 from .dnsmos.dnsmos import DNSMOS
 from .cer_wer.cer_wer import CERWER
 from .SpeechBERTScore.speech_bert_score import SpeechBERTScore
@@ -10,7 +10,7 @@ from .SpeechBLEU.speech_bleu import SpeechBLEU
 from .SpeechTokenDistance.speech_token_distance import SpeechTokenDistance
 
 __all__ = [
-    "ASRIntelligibility",
+    # "ASRIntelligibility",
     "DNSMOS",
     "CERWER",
     "SpeechBERTScore",
