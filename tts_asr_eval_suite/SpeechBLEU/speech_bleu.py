c68710167ba64ec37030ec4255a4d817f438dc96
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-07-02 17:34:56 +0900

    Update speech_bleu.py

--- tts_asr_eval_suite/SpeechBLEU/speech_bleu.py
+++ tts_asr_eval_suite/SpeechBLEU/speech_bleu.py
@@ -16,14 +16,19 @@ class SpeechBLEU:
             use_gpu=True
         )
 
-    def __call__(self, gen_audio_path):
+    def __call__(self, gt_audio_path, gen_audio_path):
+
+        gt_audio, gt_sr = torchaudio.load(gt_audio_path)
         gen_audio, gen_sr = torchaudio.load(gen_audio_path)
 
+        if gt_sr != self.sr:
+            gt_audio = torchaudio.functional.resample(gt_audio, gt_sr, self.sr)
         if gen_sr != self.sr:
             gen_audio = torchaudio.functional.resample(gen_audio, gen_sr, self.sr)
 
+        gt_audio = gt_audio[0].numpy()
         gen_audio = gen_audio[0].numpy()
 
-        score = self.metric.score(gen_audio)
+        score = self.metric.score(gt_audio, gen_audio)
 
         return score
